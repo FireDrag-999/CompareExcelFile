1e16bb2025b056b320400d46a08fc8151a5455de
FireDrag <100155709+FireDrag-999@users.noreply.github.com>
2024-04-29 21:46:48 +0100

    multiple input choice

--- CompareColumns.py
+++ CompareColumns.py
@@ -1,28 +1,29 @@
 from pandas import read_excel, ExcelFile
 
 
-fileName = input("Enter a filename in the same folder: ")
-fileName2 = input("Enter a filename in the same folder: ")
-amountOfHeaderRows = 1  # assuming that there is only one row of headers
-listOfColumns = list(read_excel(f'{fileName}.xlsx').columns)  # assuming that all column names are the same in each file
-listOfSheets = list(ExcelFile(f'{fileName}.xlsx').sheet_names)  # assuming that all sheet names are the same in each file
-
-for sheet in listOfSheets:
-    notMatchingColumn = []  # clear for each new sheet
+def checkSheet(sheet):
     print()
     targetFile = read_excel(f'{fileName}.xlsx', sheet_name=sheet, skiprows=amountOfHeaderRows)
     targetFile2 = read_excel(f'{fileName2}.xlsx', sheet_name=sheet, skiprows=amountOfHeaderRows)
-    targetFile.sort_values(ascending=True, by=targetFile.columns[0])  # sorts by first column or only column
-    targetFile2.sort_values(ascending=True, by=targetFile2.columns[0])
+    if targetFile.empty or not targetFile2.empty:
+        targetFile.sort_values(ascending=True, by=targetFile.columns[0])  # sorts by first column or only column
+        targetFile2.sort_values(ascending=True, by=targetFile2.columns[0])
+        if targetFile.equals(targetFile2):
+            print(f"Sheet: {sheet} is the same")
+        else:
+            print(f"Sheet: {sheet} is not the same")
 
-    if targetFile.equals(targetFile2):
-        print(f"Sheet: {sheet} is the same")
+        for colName in listOfColumns:
+            checkColumn(sheet, colName)
     else:
-        print(f"Sheet: {sheet} is not the same")
+        print(f'Sheet: {sheet} has no data')
+    print()
 
-    for colName in listOfColumns:
-        targetFile = read_excel(f'{fileName}.xlsx', usecols=[listOfColumns.index(colName)], skiprows=amountOfHeaderRows, sheet_name=sheet)
-        targetFile2 = read_excel(f'{fileName2}.xlsx', usecols=[listOfColumns.index(colName)], skiprows=amountOfHeaderRows, sheet_name=sheet)
+def checkColumn(sheet, colName):
+    targetFile = read_excel(f'{fileName}.xlsx', usecols=[listOfColumns.index(colName)], skiprows=amountOfHeaderRows, sheet_name=sheet)
+    targetFile2 = read_excel(f'{fileName2}.xlsx', usecols=[listOfColumns.index(colName)], skiprows=amountOfHeaderRows, sheet_name=sheet)
+
+    if not targetFile.empty or targetFile2.empty:
         targetFile.sort_values(ascending=True, by=targetFile.columns[0])
         targetFile2.sort_values(ascending=True, by=targetFile2.columns[0])
 
@@ -30,19 +31,97 @@ for sheet in listOfSheets:
             print(f"Sheet: {sheet}, Column: {colName} is the same")
         else:
             print(f"Sheet: {sheet}, Column: {colName} is not the same")
-            notMatchingColumn.append(colName)
+            if not notMatchingColumn.__contains__((sheet, colName)):
+                notMatchingColumn.append((sheet, colName))
+    else:
+        print(f'Sheet: {sheet}, Column {colName} has no data')
+
+def checkAllRows(sheet, colName):
+    counter = 0
+    targetFile = read_excel(f'{fileName}.xlsx', usecols=[listOfColumns.index(colName)], skiprows=amountOfHeaderRows, sheet_name=sheet)
+    targetFile2 = read_excel(f'{fileName2}.xlsx', usecols=[listOfColumns.index(colName)], skiprows=amountOfHeaderRows, sheet_name=sheet)
+
+    for rowNum in range(0, len(targetFile)):
+        if targetFile.values[rowNum] != targetFile2.values[rowNum]:
+            print(f"Sheet: {sheet}, Column: {colName}, row {rowNum + 2} is different: {targetFile.values[rowNum]} and {targetFile2.values[rowNum]}")  # add 1 for header and 1 as it starts at 0
+            counter += 1
+        if counter >= maxErrorRowsShown:
+            break
+
+#
+#  problems to fix: error when n, n are inputted
+#  prevent duplicates in notMatchingColumns
+
+fileName = input("Enter a filename in the same folder: ")
+fileName2 = input("Enter a filename in the same folder: ")
+amountOfHeaderRows = 1  # assuming that there is only one row of headers
+maxErrorRowsShown = 20
+listOfSheets = list(ExcelFile(f'{fileName}.xlsx').sheet_names)  # assuming that all sheet names are the same in each file
+notMatchingColumn = []  # clear for each new sheet
+
+choseColumn = input("Do you want to specifically choose a column to search column by column y/n: ")
+print()
+while choseColumn == "y":
+    print(f"Sheets: {listOfSheets}")
+    sheet = input("Enter the sheet name to check (case sensitive): ")
+    print()
+    while not listOfSheets.__contains__(sheet):
+        print("That isn't a sheet name please try again")
+        sheet = input("Enter the sheet name to check (case sensitive): ")
+        print()
+
+    listOfColumns = list(read_excel(f'{fileName}.xlsx', sheet_name=sheet).columns)  # assuming that all column names are the same in each file
+    wholeSheet = input("Check the whole sheet y/n: ")
+    if wholeSheet == "y":
+        checkSheet(sheet)
+    else:
+        print(f"Columns for {sheet}: {listOfColumns}")
+        colName = input(f"Enter the column name to check from (case sensitive): ")
+        print()
+        while not listOfColumns.__contains__(colName):
+            print("That isn't a column name please try again")
+            colName = input(f"Enter the column name to check from (case sensitive): ")
+            print()
+        checkColumn(sheet, colName)
+    choseColumn = input("Do you want to specifically choose a column to search column by column y/n: ")
     print()
 
-    skip = input(f"""Hit enter to search each row for
-Sheet: {sheet}, Columns: {notMatchingColumn}
-Type 'skip' to go to the next sheet without searching by rows: """).lower()
+skipToRows = input(f"""Do you want to search through all sheets
+These are the current (sheet,column) that aren't matching: {notMatchingColumn}
+If above is blank you must declare your own columns to check row by row y/n: """)
+print()
 
-    if skip != "skip":
-        for colName in notMatchingColumn:
-            targetFile = read_excel(f'{fileName}.xlsx', usecols=[listOfColumns.index(colName)], skiprows=amountOfHeaderRows, sheet_name=sheet)
-            targetFile2 = read_excel(f'{fileName2}.xlsx', usecols=[listOfColumns.index(colName)], skiprows=amountOfHeaderRows, sheet_name=sheet)
+if skipToRows == "y":
+    for sheet in listOfSheets:
+        listOfColumns = list(read_excel(f'{fileName}.xlsx', sheet_name=sheet).columns)
+        checkSheet(sheet)
 
-            for num in range(0, len(targetFile)):
-                if targetFile.values[num] != targetFile2.values[num]:
-                    print(
-                        f"Sheet: {sheet}, Column: {colName}, row {num + 2} is different: {targetFile.values[num]} and {targetFile2.values[num]}")  # add 1 for header and 1 as it starts at 0
+choseColumn = input("Do you want to specifically choose a column to search row by row y/n: ")
+print()
+while choseColumn == "y":
+    print(f"Sheets: {listOfSheets}")
+    sheet = input("Enter the sheet name to check (case sensitive): ")
+    print()
+    while not listOfSheets.__contains__(sheet):
+        print("That isn't a sheet name please try again")
+        sheet = input("Enter the sheet name to check (case sensitive): ")
+        print()
+
+    listOfColumns = list(read_excel(f'{fileName}.xlsx', sheet_name=sheet).columns)  # assuming that all column names are the same in each file
+    print(f"Columns {listOfColumns}")
+    colName = input(f"Enter the column name to check from (case sensitive): ")
+    print()
+    while not listOfColumns.__contains__(colName):
+        print("That isn't a column name please try again")
+        colName = input(f"Enter the column name to check from (case sensitive): ")
+        print()
+
+    checkAllRows(sheet, colName)
+    choseColumn = input("Do you want to specifically choose a column to search row by row y/n: ")
+    print()
+
+if len(notMatchingColumn) != 0:
+    for sheet, colName in notMatchingColumn:
+        checkAllRows(sheet, colName)
+
+print("Terminating")
