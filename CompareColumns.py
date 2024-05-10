955e4613226f960a9504f1de1cf40937c9a6c43f
FireDrag <100155709+FireDrag-999@users.noreply.github.com>
2024-05-10 18:32:06 +0100

    fixed list of columns error, added a sum and count of each row, used os to auto input file names

--- CompareColumns.py
+++ CompareColumns.py
@@ -1,6 +1,7 @@
 from pandas import read_excel, ExcelFile
 from logging import basicConfig, log, INFO
 import logging
+import os
 
 basicConfig(level=logging.INFO, filename="log", filemode="w", format="%(message)s")
 
@@ -8,8 +9,8 @@ basicConfig(level=logging.INFO, filename="log", filemode="w", format="%(message)
 
 def checkSheet(sheet):
     listOfColumns = listOfColumnsPerSheet[listOfSheets.index(sheet)][1]
-    targetFile = read_excel(f'{fileName}.xlsx', sheet_name=sheet)
-    targetFile2 = read_excel(f'{fileName2}.xlsx', sheet_name=sheet)
+    targetFile = read_excel(f'{path}\\{fileName}', sheet_name=sheet)
+    targetFile2 = read_excel(f'{path}\\{fileName2}', sheet_name=sheet)
     if not targetFile.empty or not targetFile2.empty:
         targetFile.sort_values(ascending=True, by=targetFile.columns[0])  # sorts by first column or only column
         targetFile2.sort_values(ascending=True, by=targetFile2.columns[0])
@@ -26,8 +27,9 @@ def checkSheet(sheet):
 
 
 def checkColumn(sheet, colName):
-    targetFile = read_excel(f'{fileName}.xlsx', usecols=[listOfColumns.index(colName)], sheet_name=sheet)
-    targetFile2 = read_excel(f'{fileName2}.xlsx', usecols=[listOfColumns.index(colName)], sheet_name=sheet)
+    listOfColumns = listOfColumnsPerSheet[listOfSheets.index(sheet)][1]
+    targetFile = read_excel(f'{path}\\{fileName}', usecols=[listOfColumns.index(colName)], sheet_name=sheet)
+    targetFile2 = read_excel(f'{path}\\{fileName2}', usecols=[listOfColumns.index(colName)], sheet_name=sheet)
 
     if not targetFile.empty or not targetFile2.empty:
         targetFile.sort_values(ascending=True, by=targetFile.columns[0])
@@ -39,6 +41,12 @@ def checkColumn(sheet, colName):
             print(f"Sheet: {sheet}, Column: {colName} is not the same"), log(level=INFO, msg=f"Sheet: {sheet}, Column: {colName} is not the same")
             print(f"First file {sheet}, {colName}: has the length {len(targetFile)}"), log(level=INFO, msg=f"First file {sheet}, {colName}: has the length {len(targetFile)}")
             print(f"Second file {sheet}, {colName}: has the length {len(targetFile2)}"), log(level=INFO, msg=f"Second file {sheet}, {colName}: has the length {len(targetFile2)}")
+            try:
+                print(f"First file {sheet}, {colName}: has the sum {sum(targetFile.values())}"), log(level=INFO, msg=f"First file {sheet}, {colName}: has the sum {sum(targetFile.values())}")
+                print(f"Second file {sheet}, {colName}: has the sum {sum(targetFile2.values())}"), log(level=INFO, msg=f"Second file {sheet}, {colName}: has the sum {sum(targetFile2.values())}")
+            except TypeError:
+                print(f"Sheet: {sheet}, {colName}: is text"), log(level=INFO, msg=f"Sheet: {sheet}, {colName}: is text")
+
             print(), log(level=INFO, msg="")
             if not notMatchingColumn.__contains__((sheet, colName)):
                 notMatchingColumn.append((sheet, colName))
@@ -47,9 +55,10 @@ def checkColumn(sheet, colName):
 
 
 def checkAllRows(sheet, colName):
+    listOfColumns = listOfColumnsPerSheet[listOfSheets.index(sheet)][1]
     counter = 0
-    targetFile = read_excel(f'{fileName}.xlsx', usecols=[listOfColumns.index(colName)], sheet_name=sheet)
-    targetFile2 = read_excel(f'{fileName2}.xlsx', usecols=[listOfColumns.index(colName)], sheet_name=sheet)
+    targetFile = read_excel(f'{path}\\{fileName}', usecols=[listOfColumns.index(colName)], sheet_name=sheet)
+    targetFile2 = read_excel(f'{path}\\{fileName2}', usecols=[listOfColumns.index(colName)], sheet_name=sheet)
 
     for rowNum in range(0, len(targetFile)):
         if str(targetFile.values[rowNum]) != str(targetFile2.values[rowNum]):
@@ -60,14 +69,27 @@ def checkAllRows(sheet, colName):
 
 
 # MAIN
-fileName = input("Enter a filename in the same folder: ")
-fileName2 = input("Enter a second filename in the same folder: ")
+path = os.getcwd() + "\\files"
+if not os.path.exists(path):
+    os.mkdir(path)
+    print(f"Please add the files to the files folder: ")
+    exit()
+
+listOfFiles = []
+count = 2
+for file in os.listdir(path):
+    if file.endswith(".xlsx") and count > 0:
+        listOfFiles = listOfFiles + [file]
+        count -= 1
+
+fileName = listOfFiles[0]
+fileName2 = listOfFiles[1]
 maxErrorRowsShown = 20
-listOfSheets = list(ExcelFile(f'{fileName}.xlsx').sheet_names)  # assuming that all sheet names are the same in each file
+listOfSheets = list(ExcelFile(f'{path}\\{fileName}').sheet_names)  # assuming that all sheet names are the same in each file
 listOfColumnsPerSheet = []
 notMatchingColumn = []  # clear for each new sheet
 for sheet in listOfSheets:
-    listOfColumns = list(read_excel(f'{fileName}.xlsx', sheet_name=sheet).columns)
+    listOfColumns = list(read_excel(f'{path}\\{fileName}', sheet_name=sheet).columns)
     listOfColumnsPerSheet.append((sheet, listOfColumns))
 
 checkAll = input(f"Do you want a summary of all sheets and their column? y/n: ")
@@ -83,8 +105,8 @@ if checkAll == "y":
             print()
 
 choseColumn = input("Do you want to search the whole column y/n: ")
-print()
 while choseColumn == "y":
+    print()
     print(f"Sheets: {listOfSheets}")
     sheet = input("Enter the sheet name to check (case sensitive): ")
     print()
@@ -106,11 +128,10 @@ while choseColumn == "y":
             print()
         checkColumn(sheet, colName)
     choseColumn = input("Do you want to search the whole column y/n: ")
-    print()
 
 choseColumn = input("Do you want to search each row y/n: ")
-print()
 while choseColumn == "y":
+    print()
     print(f"Sheets: {listOfSheets}")
     sheet = input("Enter the sheet name to check (case sensitive): ")
     print()
@@ -125,7 +146,6 @@ while choseColumn == "y":
     while not listOfColumns.__contains__(colName):
         print("That isn't a column name please try again")
         colName = input(f"Enter the column name to check from (case sensitive): ")
-        print()
 
     checkAllRows(sheet, colName)
     choseColumn = input("Do you want to search each row y/n: ")
